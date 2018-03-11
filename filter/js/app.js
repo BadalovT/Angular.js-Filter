8aec073d45c419cecd960c7563e84b063386fcae
BadalovT <bedelov.tebriz@gmail.com>
2018-03-11 03:05:35 +0300

    Add files via upload

---
+++ filter/js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("App",[]);
+app.controller("MainController",function ($scope) {
+    $scope.masinlar=[
+{"ad":"Mersedes","il":2015},
+{"ad":"BMW","il":2011},
+{"ad":"Ferrari","il":2018},
+{"ad":"Tesla","il":2017},
+{"ad":"Audi","il":2005}
+
+
+    ];
+});
